7012e0e0039bcf6398316b03cc7272b464af7c19
Rattan Priya <rpriya@yahoo-inc.com>
2015-01-06 19:58:36 -0800

    Added longest palindrome naive approach

---
+++ strings/longestPalindromBrute.js
@@ -0,0 +1,37 @@
+
+n findLongstPal(str) {
+        debugger;
+            var n = str.length,
+                        i, j,
+                                substr, isPalin, maxLen = 0,
+                                        strLen,
+                                                len = 0,
+                                                        result;
+
+                for (i = 0; i < n; i++) {
+                            for (j = i + 1; j < n; j++) {
+                                            substr = str.substring(i, j);
+                                                        isPalin = isPalindrome(substr);
+                                                                    if (isPalin) {
+                                                                                        strLen = substr.length;
+                                                                                                        if (strLen >= maxLen) {
+                                                                                                                                maxLen = strLen;
+                                                                                                                                                    result = substr;
+                                                                                                                                                                    }
+
+                                                                                                                    }
+
+                                                                            }
+                                }
+                    return result;
+}
+function isPalindrome(substr) {
+        var n = substr.length;
+            for (i = 0; i < n / 2 + 1; i++) {
+                        if (substr.charAt(i) !== substr.charAt(n - i - 1)) {
+                                        return false;
+                                                }
+                            }
+                return true;
+}
+findLongstPal('affaptbbcracecarf');
