b32645aa3d378f17d79ef8fe87d6b8ee5e6c7e17
Rattan Priya <rpriya@yahoo-inc.com>
2015-02-17 16:11:22 -0800

    Fixed the code with meaningful variable names

---
+++ javascript/strings/longestPalindrome.js
@@ -0,0 +1,37 @@
+(function () {
+    var expandAroundCenter = function expandAroundCenter(str, lIndex, rIndex) {
+        var leftIndex = lIndex,
+            rightIndex = rIndex,
+            len = str.length;
+        while (leftIndex >= 0 && rightIndex <= len - 1 && str[leftIndex] === str[rightIndex]) {
+            leftIndex--;
+            rightIndex++;
+        }
+        return str.substr(leftIndex + 1, rightIndex - leftIndex - 1);
+    };
+
+    var longestPalindromeSimple = function longestPalindromeSimple(str) {
+        var len = str.length, longest;
+        if (len === 0) {
+            return '';
+        }
+
+        longest = str[0];  // a single char itself is a palindrome
+for (var i = 0; i < len - 1; i++) {
+    var singleCharPalindrome = expandAroundCenter(str, i, i);
+    if (singleCharPalindrome.length > longest.length) {
+        longest = singleCharPalindrome;
+    }
+
+
+    var strPalindromeFromNextChar = expandAroundCenter(str, i, i + 1);
+    if (strPalindromeFromNextChar.length > longest.length) {
+        longest = strPalindromeFromNextChar;
+    }
+
+}
+return longest;
+};
+
+longestPalindromeSimple('bananad'); // => anana
+})();
