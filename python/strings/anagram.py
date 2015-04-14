59ec8267611781f459d0774ca149e50e173c8f55
Rattan Priya <rpriya@yahoo-inc.com>
2015-04-13 20:52:21 -0700

    Added the anagram list creation string

---
+++ python/strings/anagram.py
@@ -0,0 +1,18 @@
+def bucketizeAnagrams(input):
+	map = {}
+	for i in input:
+		key = ''.join(sorted(i))
+
+		if key not in map:
+			map[key] = []
+	
+		map[key].append(i) 
+
+	output = []
+	for key in map.keys():
+		output.append(map[key])
+	return output
+
+if __name__ == '__main__':
+	input =  ["star", "rats", "car", "arc", "arts", "stars"] 
+	print bucketizeAnagrams(input)
